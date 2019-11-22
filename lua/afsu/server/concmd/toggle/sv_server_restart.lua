8c4e8f68a4d046fb8a54c51818568b95045f1e5d
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2019-11-22 03:58:28 -0300

    Modified server restart toggle
    Separated BeginRestart into RoutinaryRestart and PlayerRestart
    Renamed CancelRestart to StopRestart

--- lua/afsu/server/concmd/toggle/sv_server_restart.lua
+++ lua/afsu/server/concmd/toggle/sv_server_restart.lua
@@ -2,36 +2,40 @@
 local RestartConVar = CreateConVar("afsu_server_restart", "1", FCVAR_ARCHIVE)
 local Restart = RestartConVar:GetBool()
 local SendMessage = AFSU.SendMessage
+local Humans = player.GetHumans
 
 local function RestartServer()
 	SendMessage(nil, "Info", "Restarting the server!")
 	RunConsoleCommand("_restart")
 end
 
-local function CancelRestart()
-	if timer.Exists("AFSU Server Restart") then
-		timer.Remove("AFSU Server Restart")
-	end
+local function RoutinaryRestart()
+	if next(Humans()) then return end
+
+	local UptimeDiff = AFSU.FirstRestartDelay - CurTime()
+	local Delay = UptimeDiff > 0 and UptimeDiff or AFSU.RestartDelay
+
+	timer.Create("AFSU Server Restart", Delay, 1, RestartServer)
 end
 
-local function BeginRestart(Delay)
-	local Forced = isnumber(Delay)
-
-	Delay = Forced and Delay or AFSU.RestartDelay
-
+local function PlayerRestart()
 	timer.Simple(1, function()
-		if Forced or not next(player.GetHumans()) then
-			timer.Create("AFSU Server Restart", Delay, 1, RestartServer)
-		end
+		if next(Humans()) then return end
+
+		timer.Create("AFSU Server Restart", AFSU.RestartDelay, 1, RestartServer)
 	end)
+end
+
+local function StopRestart()
+	timer.Remove("AFSU Server Restart")
 end
 
 local function Initialize()
 	if Restart then
-		BeginRestart(AFSU.FirstRestartDelay)
+		RoutinaryRestart()
 
-		hook.Add("PlayerInitialSpawn", "AFSU Player Connect", CancelRestart)
-		hook.Add("PlayerDisconnected", "AFSU Player Disconnect", BeginRestart)
+		hook.Add("PlayerInitialSpawn", "AFSU Player Connect", StopRestart)
+		hook.Add("PlayerDisconnected", "AFSU Player Disconnect", PlayerRestart)
 	end
 
 	hook.Remove("Initialize", "AFSU Init Restart Server")
@@ -47,12 +51,12 @@ cvars.AddChangeCallback("afsu_server_restart", function()
 	Restart = not Restart
 
 	if Restart then
-		BeginRestart()
+		RoutinaryRestart()
 
-		hook.Add("PlayerInitialSpawn", "AFSU Player Connect", CancelRestart)
-		hook.Add("PlayerDisconnected", "AFSU Player Disconnect", BeginRestart)
+		hook.Add("PlayerInitialSpawn", "AFSU Player Connect", StopRestart)
+		hook.Add("PlayerDisconnected", "AFSU Player Disconnect", PlayerRestart)
 	else
-		timer.Remove("AFSU Server Restart")
+		StopRestart()
 
 		hook.Remove("PlayerInitialSpawn", "AFSU Player Connect")
 		hook.Remove("PlayerDisconnected", "AFSU Player Disconnect")
